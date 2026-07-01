a2a73ea15e85a3e6d634807b122762553e442f0b
Deepika Choudhary <159138910+Deepikachoudhary05@users.noreply.github.com>
2026-07-01 16:36:05 +0530

    Time: 0 ms (100%), Space: 20.3 MB (94.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,11 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        freq = {}
 
-        for num in nums:
-            freq[num] = freq.get(num, 0) + 1
-
-        for i in range(n + 1):
-            if i not in freq:
-                return i
+        return n*(n+1)//2 - sum(nums)
+        
